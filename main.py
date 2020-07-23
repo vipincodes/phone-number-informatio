e306d70aa20fdd28580d67c49e30fa7c1a8bde1b
Vipin Sharma <39674886+vipincodes@users.noreply.github.com>
2020-07-23 12:44:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import phonenumbers
 from phonenumbers import carrier
 from phonenumbers import geocoder
 
-phone_number = phonenumbers.parse("+919919479334")
+phone_number = phonenumbers.parse("Enter number here with country code")
 
 print(geocoder.description_for_number(phone_number, 'en'))
 
